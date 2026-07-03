e4ecee028236a63e22671faa8c4a582abeb7b4ac
Akash <akashk0785@gmail.com>
2026-07-03 16:19:26 +0530

    server.js updated for server

--- server/server.js
+++ server/server.js
@@ -64,5 +64,11 @@ app.use("/api/messages", messageRouter)
 //conncet to MongoDB
 await connectDB()
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("server is running on PORT: " + PORT))
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, ()=> console.log("server is running on PORT: " + PORT))
+}
+
+//export server 
+export default server
+
